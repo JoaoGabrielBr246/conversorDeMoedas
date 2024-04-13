518a2630d3742ae50c9ba122f33fe0f9f72e1b42
joaogabrocha <80985064+JoaoGabrielBr246@users.noreply.github.com>
2024-04-13 12:52:59 -0300

    Add files via upload

---
+++ src/README.md
@@ -0,0 +1,3 @@
+Esse é um programa feito para o Challenge do Projeto One T6.
+Onde foi passado um conversor de moedas usando uma API, possibilitando obter informações em tempo real das moedas.
+Em conjunto, a pedido do Challenge, transformei os dados em JSON com o GSON, com informações úteis para o usuário sempre obter com clareza o que deseja.
---
+++ src/models/Conversor.java
@@ -0,0 +1,35 @@
+package models;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conversor {
+    public Moeda converterMoeda(String valor1, String valor2, double moeda){
+        String minhaKey = "543f94a7fb20d5aaba94794c";
+        URI endereco = URI.create("https://v6.exchangerate-api.com/v6/"+
+                minhaKey+"/pair/"+valor1+"/"+valor2+"?amount="+moeda);
+
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+        HttpResponse<String> response = null;
+        try {
+            response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Não consegui converter os valores :/");
+        }
+
+        return new Gson().fromJson(response.body(),Moeda.class);
+
+    }
+}
